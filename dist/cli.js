fb1c96c3eed36208a4271b25a804ef4b139c7c3a
Giedrius Grabauskas <giedrius@quatrodev.com>
2016-10-12 18:17:38 +0300

    Builded code.

--- dist/cli.js
+++ dist/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const arguments_1 = require('./arguments');
 const main_1 = require('./main');
 new main_1.default(arguments_1.default.pattern, arguments_1.default.options || {});
